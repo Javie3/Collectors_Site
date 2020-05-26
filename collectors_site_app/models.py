c3aedd3abcaf51199fc9c8fed5a0118bf72c2f34
Javie3 <62359350+Javie3@users.noreply.github.com>
2020-05-26 16:35:34 -0700

    made Change in Models.py, urls.py, views.py, and added the templates folder

--- collectors_site_app/models.py
+++ collectors_site_app/models.py
@@ -2,3 +2,22 @@ from django.db import models
 import re
 
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postdata):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postdata['pw']) < 8:
+            errors['pw'] = "password must be 8 Charcters long"
+        if len(postdata['fname']) < 2 or len(postdata['lname']) < 2:
+            errors['name'] = "First name must be at least 2 Charcters long"
+        if not EMAIL_REGEX.match(postdata['email']):
+            errors['email'] = "email must be valid"
+        return errors        
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email =  models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    objects = UserManager()
--- collectors_site_app/views.py
+++ collectors_site_app/views.py
@@ -3,5 +3,44 @@ from .models import *
 from django.contrib import messages
 import bcrypt
 
+# def index(request):
+#     return HttpResponse("It's working!")
+
+
+#Renders Index#
 def index(request):
-    return HttpResponse("It's working!")
+    return render(request, "index.html")
+
+
+  
+#process the user Info 
+def process(request):
+    print(request.POST)
+    #Creates User and makes sure password match's with if statement#
+    if request.POST['pw'] != request.POST['confpw']:
+        return redirect('/')
+    else: 
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors)>0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')   
+        new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=request.POST['pw'])
+        request.session['user'] = new_user.first_name
+        request.session['id'] = new_user.id
+        return redirect('/')
+
+
+#logins in User
+def login(request):
+    print(request.POST)
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if logged_user.password == request.POST['pw']:
+            request.session['user'] = logged_user.first_name
+            request.session['id'] = logged_user.id
+        return redirect('/success')
+    return redirect('/')
+
+  
